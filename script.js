58d4973e5e545bcdadbc243e1b00a652b62b100e
Noura Al aidy <alaidynoura370@gmail.com>
2025-11-10 10:09:52 +0200

    frist commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Array المصفوفه
+
+const langs = ["العربيه", "الانجليزيه","الفرنسيه"];
+langs[0] = "الايطاليه";
+console.log(langs);
+
+console.log(langs[2]);
+
+langs.push("العربيه");//يضيف اى حاجه انا عاوزاها
+langs.pop(); // يمسح اخر  حاجه
+
+console.log(langs);
+
+
